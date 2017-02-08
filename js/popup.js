a69af23e909d7f26eb02cc57ab865560651bea7e
mobiuschang <mobiuschang@gmail.com>
2017-02-08 14:03:43 -0500

    Fixed minor style

--- js/popup.js
+++ js/popup.js
@@ -9,10 +9,11 @@ $(document).ready(function(){
         url: "https://solace-admin.herokuapp.com/api/tokens/sessions/create",
         success: function(data){
 
-          $('body').html('<div class="form-group col-md-4"> <label for="text">What"s on your mind today: </label> <textarea class="form-control" rows="6" id="comment"></textarea><button type="button" class="btn btn-success finish-button">Finish</button></div>');
+          $('body').html('<div class="form-group col-md-4 text-form"> <label for="text">What"s on your mind today: </label> <textarea class="form-control" rows="6" id="comment"></textarea><button type="button" class="btn btn-success finish-button">Finish</button></div>');
         },
         error: function() {
-          $(".container").prepend("<div class = 'invalid'>Invalid Credential</div>")
+          $("#invalid").show();
+
         }
       });
       return false;
