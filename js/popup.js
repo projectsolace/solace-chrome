5e171aa92082049c0b4e567a55d797b5fb4cf44a
mobiuschang <mobiuschang@gmail.com>
2017-02-10 19:11:59 -0500

    Added posting texts to watson backend

--- js/popup.js
+++ js/popup.js
@@ -12,9 +12,12 @@ $(document).ready(function(){
     }
   });
 
+  $('#comment').focus(function(){
+    $('#watson-posted').hide();
+  });
+
   $('#loginForm').submit(function(){
 
-    console.log('you have clicked');
       $.ajax({
         type: "POST",
         data: $(this).serialize(),
@@ -24,7 +27,8 @@ $(document).ready(function(){
           $('.text-form').show();
           $('#login-container').hide();
 
-          chrome.storage.sync.set({'id_token': data.id_token}, function(){
+          console.log('successful sign in here is data', data.user)
+          chrome.storage.sync.set({'id_token': data.id_token, 'user': data.user}, function(){
             console.log('id_token saved in storage');
           });
 
@@ -37,11 +41,43 @@ $(document).ready(function(){
       return false;
   });
 
-  $('.signout-button').click(function(){
-      chrome.storage.sync.remove('id_token', function(){
-        console.log('Remove id_token successful');
+  $('#signout-button').click(function(){
+      chrome.storage.sync.remove(['id_token', 'user'], function(){
+        console.log('Sign out successful');
+        $('#comment').val('');
         $('.text-form').hide();
         $('#login-container').show();
+        $("#invalid").hide();
+        $('form').trigger('reset');
+
       });
+  });
+
+  $('#finish-button').click(function(){
+
+    var comment = $('#comment').val();
+    $('#watson-posted').hide();
+    chrome.storage.sync.get('user', function(result){
+
+      if(result.user) {
+         $.ajax({
+            type: "POST",
+            data: {text: comment, userID: result.user.id},
+            url: "https://solace-admin.herokuapp.com/api/watson/write",
+            success: function(data){
+              $('#comment').val('');
+              $('#watson-posted').show();
+              console.log('Successful post data to backend');
+
+            },
+            error: function() {
+              console.log('Failed to post data to backend');
+
+            }
+         });
+
+      }
+    });
+
   });
 });
