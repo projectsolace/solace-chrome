ea74380276713998450ebcfd1375be93496234bc
Jenny Chan <jrc337@cornell.edu>
2017-02-11 16:05:09 -0500

    added character count

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,9 +4,8 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
 chrome.storage.sync.get('id_token', function(result){
-  if(result.id_token === undefined) {
+  if (result.id_token === undefined) {
     sendResponse({'authenticated': false});
   } else {
     sendResponse({'authenticated': true, 'id_token': result.id_token});
@@ -14,10 +13,9 @@ chrome.storage.sync.get('id_token', function(result){
 });
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if(request.id_token){
+  if (request.id_token){
     chrome.storage.sync.set({'id_token': request.id_token}, function() {
       console.log('id_token saved in storage');
-
     });
   }
 });
